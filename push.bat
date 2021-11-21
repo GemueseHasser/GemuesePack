7ce4e4f1da45ebe553f90e58c713b29d648705c6
Link-10 <maximilianlobe6@gmail.com>
2021-11-21 11:18:31 +0100

    fix push bug

--- push.bat
+++ push.bat
@@ -2,6 +2,6 @@
 echo "Push to remote:"
 echo.
 echo.
-set /p token = "Bitte gib ein gültiges Access-Token ein: "
+set /p token="Bitte gib ein gültiges Access-Token ein: "
 echo.
 git push https://%token%@github.com/GemueseHasser/GemuesePack.git
